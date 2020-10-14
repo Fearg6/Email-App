aae88c56bed24300993b7633b07dae007a394e84
Fearghal McMorrow <fearghal.mcmorrow@gmail.com>
2020-10-14 21:10:04 +0100

    Update Email.java

--- Email-app/src/com/mcmorrow/email/app/Email.java
+++ Email-app/src/com/mcmorrow/email/app/Email.java
@@ -1,4 +1,23 @@
 package com.mcmorrow.email.app;
 
 public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+    //Constructor to receive the first name and last name
+
+    //Ask for the department
+
+    // Set mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
+
+    
+
 }
