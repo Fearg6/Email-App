a8f7036c6378ba08570f1afba6226d7e13394027
Fearghal McMorrow <fearghal.mcmorrow@gmail.com>
2020-10-28 18:38:45 +0000

    random password

--- Email-app/src/com/mcmorrow/email/app/Email.java
+++ Email-app/src/com/mcmorrow/email/app/Email.java
@@ -8,6 +8,7 @@ public class Email {
     private String password;
     private String department;
     private int mailBoxCapacity;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
 
 
@@ -18,6 +19,12 @@ public class Email {
         System.out.println("Department Codes:\n Email created: First name: "+this.firstName+" Last Name: "+this.lastName);
 
         this.department = setDepartment();
+        System.out.println("Department: "+this.department);
+
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Password: "+this.password);
+
+        //Combine elements to generate email
     }
     //Ask for the department
 
@@ -30,6 +37,17 @@ public class Email {
         else if(departmentChoice == 3){return "acct";}
         else{return "";}
 
+    }
+
+    //Generate a random password
+    private String randomPassword(int length){
+        String passwordSet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[];'#,./";
+         char[] password = new char[length];
+         for(int i=0;i<password.length;i++){
+             int rand = (int) (Math.random() * passwordSet.length());
+             password[i] = passwordSet.charAt(rand);
+         }
+        return new String(password);
     }
 
     // Set mailbox capacity
